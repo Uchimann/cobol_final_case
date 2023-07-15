5b94f987f525707a9eaf57f8fdeeb9d8f9bbb69d
İsmail Çelebi <133144077+Uchimann@users.noreply.github.com>
2023-07-15 16:21:49 +0300

    ASSIGN UPDATED

--- Z95626.CBL(MAINPROG).cbl
+++ Z95626.CBL(MAINPROG).cbl
@@ -11,51 +11,50 @@
        FILE SECTION.
        FD  OUT-FILE RECORDING MODE F.
          01  OUT-REC.
-           05 OUT-ISLEM-TIPI    PIC 9(01).
-           05 OUT-ID            PIC 9(05).
-           05 OUT-DVZ           PIC 9(03).
-           05 OUT-SPACE         PIC X(05).
-           05 OUT-RETURN-CODE   PIC 9(02).
-           05 FILLER            PIC X(01).
-           05 OUT-ACIKLAMA      PIC X(30).
-           05 OUT-SPACE2        PIC X(01).
-           05 OUT-FNAME-FROM    PIC X(15).
-           05 OUT-FNAME-TO      PIC X(15).
-           05 OUT-LNAME-FROM    PIC X(15).
-           05 OUT-LNAME-TO      PIC X(15).
+           05 OUT-ISLEM-TIPI           PIC X(01).
+           05 OUT-ID                   PIC 9(05).
+           05 OUT-DVZ                  PIC 9(03).
+           05 OUT-SPACE                PIC X(05).
+           05 OUT-RETURN-CODE          PIC 9(02).
+           05 FILLER                   PIC X(01).
+           05 OUT-DESCRIPTION          PIC X(30).
+           05 OUT-SPACE2               PIC X(01).
+           05 OUT-FNAME-FROM           PIC X(15).
+           05 OUT-FNAME-TO             PIC X(15).
+           05 OUT-LNAME-FROM           PIC X(15).
+           05 OUT-LNAME-TO             PIC X(15).
        FD  INP-FILE RECORDING MODE F.
          01  INP-REC.
-           07 INP-ISLEM-TIPI    PIC 9(01).
-           07 INP-ID            PIC 9(5).
-           07 INP-DVZ           PIC 9(3).
+           07 INP-ISLEM-TIPI           PIC X(01).
+           07 INP-ID                   PIC 9(5).
+           07 INP-DVZ                  PIC 9(3).
        WORKING-STORAGE SECTION.
          01  WS-WORK-AREA.
-           05 WS-SUBPROG       PIC X(08)       VALUE 'SUBPROG'.
-           05 INP-ST            PIC 9(02).
-              88 INP-EOF                   VALUE 10.
-              88 INP-SUCCES                VALUE 00 97.
-           05 OUT-ST            PIC 9(02).
-              88 OUT-SUCCESS               VALUE 00 97.
+           05 WS-SUBPROG               PIC X(08)  VALUE 'SUBPROG'.
+           05 INP-ST                   PIC 9(02).
+              88 INP-EOF                          VALUE 10.
+              88 INP-SUCCES                       VALUE 00 97.
+           05 OUT-ST                   PIC 9(02).
+              88 OUT-SUCCESS                      VALUE 00 97.
          01 WS-SUB-AREA.
-              07 WS-ISLEM-TIPI     PIC 9(01).
-                 88 WS-ISLEM-TIPI-VALID       VALUES 1 THRU 5.
-              07 WS-SUB-FUNC    PIC 9(01).
-                 88 WS-FUNC-OPEN           VALUE 1.
-                 88 WS-FUNC-READ           VALUE 2.
-                 88 WS-FUNC-UPDATE         VALUE 3.
-                 88 WS-FUNC-WRITE          VALUE 4.
-                 88 WS-FUNC-DELETE         VALUE 5.
-                 88 WS-FUNC-CLOSE          VALUE 9.
-              07 SUB-OUT-ISLEM-TIPI    PIC 9(01).
+              07 WS-ISLEM-TIPI         PIC X(01).
+              07 WS-SUB-FUNC           PIC 9(01).
+                 88 WS-FUNC-OPEN                  VALUE 1.
+                 88 WS-FUNC-READ                  VALUE 2.
+                 88 WS-FUNC-UPDATE                VALUE 3.
+                 88 WS-FUNC-WRITE                 VALUE 4.
+                 88 WS-FUNC-DELETE                VALUE 5.
+                 88 WS-FUNC-CLOSE                 VALUE 9.
+              07 SUB-OUT-ISLEM-TIPI    PIC X(01).
               07 SUB-OUT-ID            PIC 9(05).
               07 SUB-OUT-DVZ           PIC 9(03).
               07 SUB-OUT-RETURN-CODE   PIC 9(02).
-              07 SUB-OUT-ACIKLAMA      PIC X(30).
+              07 SUB-OUT-DESCRIPTION   PIC X(30).
               07 SUB-OUT-FNAME-FROM    PIC X(15).
               07 SUB-OUT-FNAME-TO      PIC X(15).
               07 SUB-OUT-LNAME-FROM    PIC X(15).
               07 SUB-OUT-LNAME-TO      PIC X(15).
-              07 SUB-INP-ISLEM-TIPI    PIC 9(01).
+              07 SUB-INP-ISLEM-TIPI    PIC X(01).
               07 SUB-INP-ID            PIC 9(5).
               07 SUB-INP-DVZ           PIC 9(3).
        PROCEDURE DIVISION.
@@ -82,21 +81,28 @@
        H100-END. EXIT.
 
        H200-PROCCES.
-           MOVE INP-ISLEM-TIPI  TO SUB-INP-ISLEM-TIPI
-           MOVE INP-ID          TO SUB-INP-ID
-           MOVE INP-DVZ         TO SUB-INP-DVZ
+       
+           MOVE INP-ISLEM-TIPI              TO SUB-INP-ISLEM-TIPI
+           MOVE INP-ID                      TO SUB-INP-ID
+           MOVE INP-DVZ                     TO SUB-INP-DVZ
 
-           IF INP-ISLEM-TIPI < 6
-              CALL WS-SUBPROG USING WS-SUB-AREA
-           ELSE
-              DISPLAY 'YAPAMAZSIN'
-           END-IF.
+              IF INP-ISLEM-TIPI = 'R'
+                 CALL WS-SUBPROG USING WS-SUB-AREA
+              ELSE IF INP-ISLEM-TIPI = 'D'
+                 CALL WS-SUBPROG USING WS-SUB-AREA
+              ELSE IF INP-ISLEM-TIPI = 'W'
+                 CALL WS-SUBPROG USING WS-SUB-AREA
+              ELSE IF INP-ISLEM-TIPI = 'U'
+                 CALL WS-SUBPROG USING WS-SUB-AREA
+              ELSE
+                 DISPLAY 'GECERSIZ ISLEM TIPI' INP-ISLEM-TIPI
+              END-IF.
 
            MOVE SUB-OUT-ISLEM-TIPI          TO   OUT-ISLEM-TIPI
            MOVE SUB-OUT-ID                  TO   OUT-ID
            MOVE SUB-OUT-DVZ                 TO   OUT-DVZ
            MOVE SUB-OUT-RETURN-CODE         TO   OUT-RETURN-CODE
-           MOVE SUB-OUT-ACIKLAMA            TO   OUT-ACIKLAMA
+           MOVE SUB-OUT-DESCRIPTION         TO   OUT-DESCRIPTION
            MOVE SUB-OUT-FNAME-FROM          TO   OUT-FNAME-FROM
            MOVE SUB-OUT-FNAME-TO            TO   OUT-FNAME-TO
            MOVE SUB-OUT-LNAME-FROM          TO   OUT-LNAME-FROM
@@ -105,7 +111,7 @@
            MOVE SUB-INP-ID                  TO   INP-ID
            MOVE SUB-INP-DVZ                 TO   INP-DVZ
            MOVE ' RC: '                     TO   OUT-SPACE
-           MOVE '-'                         TO   OUT-SPACE2 
+           MOVE '-'                         TO   OUT-SPACE2
            WRITE OUT-REC.
               READ INP-FILE.
        H200-END. EXIT.
